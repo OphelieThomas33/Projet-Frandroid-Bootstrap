1b8cae9b7558d26067c09b423244f2ba71258903
OphelieThomas33 <ophelie_thomas@hotmail.com>
2022-10-25 11:25:05 +0200

    Add light and dark mode

--- script.js
+++ script.js
@@ -9,3 +9,34 @@ document.addEventListener('scroll', (e) => {
         btnScrollTop.style.display = "none";
     }
   });
+
+  document.querySelectorAll(".changethemediv").forEach(div => {
+    div.addEventListener("click", event => {
+        let themeTarget = event.currentTarget.dataset.theme;
+
+            if(themeTarget == "light") {
+
+                document.querySelectorAll(".bg-dark").forEach(darkElement => {
+                    darkElement.classList.remove("bg-dark");
+                    darkElement.classList.add("bg-light");
+                });
+
+                document.querySelectorAll(".text-light").forEach(darkElement => {
+                    darkElement.classList.remove("text-light");
+                    darkElement.classList.add("text-dark");
+                });
+            }
+            else if(themeTarget == "dark") {
+                document.querySelectorAll(".bg-light").forEach(darkElement => {
+                    darkElement.classList.remove("bg-light");
+                    darkElement.classList.add("bg-dark");
+                });
+
+                document.querySelectorAll(".text-dark").forEach(darkElement => {
+                    darkElement.classList.remove("text-dark");
+                    darkElement.classList.add("text-light");
+                });
+
+            }
+    });
+}) 
